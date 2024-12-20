31141fce87ec71fb6d09eee111e426c4ce73cb77
Kabil Rajendran <kabilamoorthi@gmail.com>
2024-12-20 12:35:31 +0530

    my first js prject for web to lead form in salesforce

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("linked")
+
+function beforesubmit(){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log(outputdate,inputdate);
+    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatteddate;
+}
