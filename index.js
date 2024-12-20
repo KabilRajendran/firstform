bae8fc1a86502c256908de048ebe346cd114ab00
Kabil Rajendran <kabilamoorthi@gmail.com>
2024-12-20 12:39:43 +0530

    Revert "my first js prject for web to lead form in salesforce"
    This reverts commit 31141fce87ec71fb6d09eee111e426c4ce73cb77.

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("linked")
-
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log(outputdate,inputdate);
-    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formatteddate;
-}
